38dd3a61bca9eafea501972a6f4eb235f35243ca
frytaz <frytaz@gmail.com>
2025-08-21 11:03:38 +0200

    feat(web): custom 404 page

---
+++ src/theme/NotFound/index.tsx
@@ -0,0 +1,19 @@
+import React, { type ReactNode } from 'react';
+import Layout from '@theme/Layout';
+import NotFoundContent from '@theme/NotFound/Content';
+import { PageMetadata } from '@docusaurus/theme-common';
+import { translate } from '@docusaurus/Translate';
+
+export default function Index(): ReactNode {
+  const title = translate({
+    id: 'theme.NotFound.title',
+    message: 'Page Not Found',
+  });
+  return (
+    <>
+      <Layout>
+        <NotFoundContent />
+      </Layout>
+    </>
+  );
+}

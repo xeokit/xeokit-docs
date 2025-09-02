700f1ab9d7a0920e290f68dfec69dd9bf8c7de3d
Marcin <frytaz@gmail.com>
2025-09-02 16:18:18 +0200

    Merge from xeokit-docs/develop (#34)
    * Delete: blog/2021-02-11-new-website directory
    
    * feat: webcomponents blog posts
    
    * chore: update authors list (#26)
    
    * chore: remove TODO section and update docusaurus config to enable indexing
    
    * feat: add default Nginx configuration for production Docker setup
    
    * feat: add robots.txt for search engine indexing
    
    * Feat/xv 877 improvements (#30)
    
    * feat: custom 404 page
    
    * chore: footer links
    
    * chore: blog posts truncate tags, videos
    
    * fix: section links, user logos
    
    * feat: git lfs attributes
    
    * feat: add Google Analytics
    
    * fix: fix titles and add basic descriptions
    
    * feat: add ms clarity
    
    * feat: replace docusaurus social card image with xeokit social image
    
    * Feat/xv 877 improvements (#33)
    
    * feat(web): custom 404 page
    
    * chore: footer links
    
    * chore: blog posts truncate tags, videos
    
    * fix: section links, user logos
    
    * feat: git lfs attributes
    
    * feat: navbar links
    
    * chore: webcomponents blog post update
    
    * chore: merge pnpm
    
    ---------
    
    Co-authored-by: Diyan Rashevski <154256183+drashevski@users.noreply.github.com>
    Co-authored-by: damianbobrowski <damian.bobrowski@creoox.com>
    Co-authored-by: Dominik Szaradowski <d.szaradowski@gmail.com>
    Co-authored-by: Dominik Szaradowski <20087092+DhivinX@users.noreply.github.com>

--- src/pages/sdk-v2/examples/xeokit-bim-viewer.tsx
+++ src/pages/sdk-v2/examples/xeokit-bim-viewer.tsx
@@ -1,15 +1,12 @@
 import Layout from '@theme/Layout';
 import type { ReactNode } from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import XeoIframe from '@site/src/components/XeoIframe';
 
 export default function Hello(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
-
   return (
     <Layout
-      title={`xeokit-sdk - ${siteConfig.title}`}
-      description="BIM Viewer Examples">
+      title={`BIM Viewer Examples`}
+      description="BIMViewer is an open source 2D/3D BIM viewer that runs in the browser and loads models from your file system.">
       <XeoIframe src={'https://xeokit.github.io/xeokit-sdk/examples/bimviewer.html'} />
     </Layout>
   );
--- src/pages/sdk-v3/development-status.tsx
+++ src/pages/sdk-v3/development-status.tsx
@@ -1,14 +1,11 @@
 import Layout from '@theme/Layout';
 import type { ReactNode } from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import XeoIframe from '@site/src/components/XeoIframe';
 
 export default function Hello(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
-
   return (
     <Layout
-      title={`xeokit-sdk - ${siteConfig.title}`}
+      title={`Development Status`}
       description="Dev Status">
       <XeoIframe src="https://xeokit.github.io/sdk/userguide/dev-status/" />
     </Layout>
